a1868df6f8865fdf281def2225ce819a2e2696d5
Ashish <Ashish@192.168.7.81>
2021-11-17 13:59:32 +0530

    Create complex form for student and map parameter using PathVariable annot.

--- springmvcsearch/src/main/java/springmvcsearch/SearchController.java
+++ springmvcsearch/src/main/java/springmvcsearch/SearchController.java
@@ -1,19 +1,31 @@
 package springmvcsearch;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class SearchController {
-	
+
+	//using PathVariable annotation we can Map our parameter
+	@RequestMapping("/user/{userId}/{userName}")
+	public String getUserDetails(@PathVariable("userId") int userId, @PathVariable("userName") String userName) {
+
+		System.out.println(userId);
+		System.out.println(userName);
+
+		return "home";
+
+	}
+
 	@RequestMapping("/home")
 	public String home() {
 		System.out.println("Going to home view");
 		return "home";
 	}
-	
+
 //	@RequestMapping("/search")
 //	public RedirectView search(@RequestParam("quarybox") String query) {
 //		String url = "https://www.google.com/search?q="+query;
@@ -23,16 +35,16 @@ public class SearchController {
 //		return redirectView;
 //		
 //	}
-	
+
 	@RequestMapping("/search")
 	public RedirectView search(@RequestParam("quarybox") String query) {
-		//String url;
+		// String url;
 		RedirectView redirectView = new RedirectView();
-		
-		redirectView.setUrl("https://www.google.com/search?q="+query);
-		
+
+		redirectView.setUrl("https://www.google.com/search?q=" + query);
+
 		return redirectView;
-		
+
 	}
 
 }
